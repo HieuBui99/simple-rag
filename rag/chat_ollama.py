6d432acc0c714aab6b421936212aaf4163c6b638
HieuBui99 <hieu25121999@gmail.com>
2025-01-29 10:14:27 +0900

    async

--- rag/chat_ollama.py
+++ rag/chat_ollama.py
@@ -15,15 +15,15 @@ load_dotenv()
 
 def split_text(text):
     # Split the text into the model's thought and the rest of the text
-    pattern = r'<think>(.*?)</think>\s*(.*)'
+    pattern = r"<think>(.*?)</think>\s*(.*)"
     match = re.search(pattern, text, re.DOTALL)
-    
+
     if not match:
         return None, None
-        
+
     thought = match.group(1).strip()
     rest = match.group(2).strip()
-    
+
     return thought, rest
 
 
@@ -34,32 +34,30 @@ async def stream_chat_response(url: str, payload: Dict) -> AsyncGenerator[bytes,
                 yield data
 
 
+async def get_chat_response(url: str, payload: Dict) -> Optional[Dict]:
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=payload) as response:
+            response_data = await response.read()
+    return response_data
+
+
 async def main():
     url = os.getenv("OLLAMA_ENDPOINT")
-    payload =  {
+    payload = {
         "model": "deepseek-r1:14b",
         "messages": [
-            {
-                "role": "system",
-                "content": "You are mario from the game mario bros."
-            },
-            {
-                "role": "user",
-                "content": "Hello, who are you"
-            }
+            {"role": "system", "content": "You are mario from the game mario bros."},
+            {"role": "user", "content": "Hello, who are you"},
         ],
         "options": {
             "temperature": 0.5,
-        },
-        "stream": True
+        }
     }
-    buffer = BytesIO()
-    async for chunk in stream_chat_response(url, payload):
-        buffer.write(chunk)
-    buffer.seek(0)
+    response = await get_chat_response(url, payload)
+
     response_data = ""
     # decode response content
-    for line in buffer.read().decode("utf-8").split("\n"):
+    for line in response.decode("utf-8").split("\n"):
         if line:
             response_json = json.loads(line)
             response_data += response_json["message"]["content"]
@@ -68,7 +66,8 @@ async def main():
 
     return thought, answer
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     thought, answer = loop.run_until_complete(main())
     print(thought)
