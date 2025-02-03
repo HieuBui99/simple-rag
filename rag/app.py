badc7c75a049af3f581e21dbbf26d9dbe8859b5e
HieuBui99 <hieu25121999@gmail.com>
2025-02-03 23:42:27 +0900

    feat: BM25 and faiss vector search

--- rag/app.py
+++ rag/app.py
@@ -1,7 +1,40 @@
-from fastapi import FastAPI
-from fastapi import Request
+from contextlib import asynccontextmanager
+from pathlib import Path
+
+import re
+import faiss
+import pandas as pd
+from fastapi import FastAPI, Request
+from rank_bm25 import BM25Okapi
+
+from .settings import settings
+
+
+search_index = {}
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Initializing vector search index!!")
+
+    document_path = Path(settings.DATA_PATH) / "anime_clean.csv"
+    documents = pd.read_csv(document_path)['text']
+
+    #BM25
+    tokenized_documents = [re.sub(r"[^a-zA-Z0-9]", " ", document).lower().split() for document in documents]
+    lexical_index = BM25Okapi(tokenized_documents)
+
+    #Faiss
+    semantic_index = faiss.read_index(settings.DATA_PATH / "anime.faiss") 
+
+    search_index['lexical'] = lexical_index
+    search_index['semantic'] = semantic_index
+
+    yield
+
+    search_index.clear()
 
 app = FastAPI()
+
 
 @app.get("/health_check")
 async def health_check(request: Request):
---
+++ rag/settings.py
@@ -0,0 +1,46 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    """
+    Config settings for the application
+    """
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.load_settings()
+        return cls._instance
+    
+    def load_settings(self):    
+        self._OLLAMA_ENDPOINT: str = os.environ.get("OLLAMA_ENDPOINT")
+        self._OLLAMA_TEMPERATURE: float = float(os.environ.get("OLLAMA_TEMPERATURE"))
+        self._VECTORIZE_ENDPOINT: str = os.environ.get("VECTORIZE_ENDPOINT")
+        self._RERANK_ENDPOINT: str = os.environ.get("RERANK_ENDPOINT")
+        self._DATA_PATH: str = os.environ.get("DATA_PATH")
+
+    @property
+    def OLLAMA_ENDPOINT(self) -> str:
+        return self._OLLAMA_ENDPOINT
+    
+    @property
+    def OLLAMA_TEMPERATURE(self) -> float:
+        return self._OLLAMA_TEMPERATURE
+    
+    @property
+    def VECTORIZE_ENDPOINT(self) -> str:
+        return self._VECTORIZE_ENDPOINT
+    
+    @property
+    def RERANK_ENDPOINT(self) -> str:
+        return self._RERANK_ENDPOINT
+    
+    @property
+    def DATA_PATH(self) -> str:
+        return self._DATA_PATH
+    
+
+settings = Settings()
